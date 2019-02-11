b1df5d275915948ed6d1b69b6c28e760ecbb2c8f
Tommy <mcdermot@oregonstate.edu>
2019-02-11 13:49:35 -0500

    First Commit.  Working first dropdown.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sports-app';
+export class AppComponent implements OnInit {
+  sports = [  'MLB', 'NFL', 'NBA', 'NHL'  ];
+
+  currentSport: string = 'Sport';
+  sportsData: object;
+  teams: object;
+  show: boolean = false;
+
+  getSports() {
+    return this.sports;
+  }
+
+  getTeams(sport: any) {
+    console.log(sport);
+    this.currentSport = sport;
+    this.show = true;
+  }
+
+  constructor(private http: HttpClient) {
+    this.currentSport = '';
+    this.getSports();
+  }
+
+  ngOnInit(): void {
+    this.http.get<Object>('./assets/data.json').subscribe(
+      data => {
+        this.sportsData = data;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
